c87b0d9f5669794982dc24ce3c926a99451c2265
Mobasshir Bhuiyan <mobasshirbhuiyan.shagor@gmail.com>
2021-06-22 10:53:33 +0600

    Create demo.py

---
+++ languages/python/context_manager/demo.py
@@ -0,0 +1,26 @@
+import os
+from contextlib import contextmanager
+
+class Change_Dir():
+  def __init__(self, destination):
+    pass
+  def __enter__(self):
+    pass
+  def __exit__(self):
+    pass
+
+@contextmanager
+def change_dir(destination):
+    try:
+        cwd = os.getcwd()
+        os.chdir(destination)
+        yield
+    finally:
+        os.chdir(cwd)
+
+
+with change_dir('Sample-Dir-One'):
+    print(os.listdir())
+
+with change_dir('Sample-Dir-Two'):
+    print(os.listdir())
